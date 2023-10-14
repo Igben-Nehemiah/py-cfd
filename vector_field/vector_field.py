e26bc837f782147263c2d9df1917ed4fa75b160f
Nehemiah Igben <igbennehemiah@gmail.com>
2023-10-14 17:42:48 +0100

    updated point(s) to position(s)

--- vector_field/vector_field.py
+++ vector_field/vector_field.py
@@ -1,6 +1,6 @@
 import numpy as np
-from scipy.interpolate import RegularGridInterpolator
-from grid_range import GridRange
+from scipy.interpolate import RegularGridInterpolator, interpn
+from vector_field.grid_range import GridRange
 from typing import Sequence, Callable
 import inspect
 
@@ -15,34 +15,55 @@ class VectorField:
 
     Attributes
     ----------
-        mesh_points (List[np.ndarray]): List of grid points along each dimension.
-        mesh_grids (Tuple[np.ndarray]): Mesh grids created from grid points.
-        interpolators (List[RegularGridInterpolator]): Interpolators for each grid.
+        mesh_positions (List[np.ndarray]): List of grid positions along each dimension.
+        mesh_grids (Tuple[np.ndarray]): Mesh grids created from grid positions.
 
     Example
     -------
     ```
-        # Create a VectorField with two grid dimensions, each ranging from 0 to 5 with 6 points.
-        vf = VectorField([GridRange(0, 5, 6), GridRange(0, 5, 6)])
+    # Create a VectorField with two grid dimensions, each ranging from 0 to 5 with 6 positions.
+    vf = VectorField([GridRange(0, 5, 6), GridRange(0, 5, 6)])
 
-        # Set the VectorField from a function that computes vector values.
-        vf.set_from_function(lambda args: (args[0]**2, args[1]**2))
+    # Set the VectorField from a function that computes vector values.
+    vf.set_from_function(lambda x, y: (x**2, y**2))
 
-        # Get vectors at specified points.
-        vectors = vf.get_vectors_at_points([(1, 2), (2, 4), (2, 2), (3, 1)])
+    # Get vectors at specified positions.
+    vectors = vf.get_vectors_at_positions([(1, 2), (2, 4), (2, 2), (3, 1)])
     ```
     """
 
     def __init__(self, grid_ranges: list[GridRange]):
-        # Generate grid points from grid ranges
-        self.mesh_points = [np.linspace(*grid_range.as_tuple())
-                            for grid_range in grid_ranges]
+        # Generate grid positions from grid ranges
+        self.mesh_positions = [np.linspace(*grid_range.as_tuple())
+                               for grid_range in grid_ranges]
 
-        # Create mesh grids from the grid points
-        self.mesh_grids = np.meshgrid(*self.mesh_points, indexing='ij')
+        self.__grid_ranges = grid_ranges
 
-        # Initialize interpolators
-        self.interpolators = None
+        # Create mesh grids from the grid positions
+        self.mesh_grids = np.meshgrid(*self.mesh_positions, indexing='ij')
+
+    @property
+    def dim(self):
+        return len(self.mesh_grids)
+
+    def set_grid_values(self, mesh_grids_values: np.ndarray) -> None:
+        """
+        Set the VectorField's grid values directly from a collection of mesh grid values.
+
+        Parameters
+        ----------
+            mesh_grids_values (np.ndarray): A collection of mesh grid values for each dimension.
+
+        Raises
+        ------
+            ValueError: If the provided mesh grid values do not match the number of grid dimensions.
+        """
+        if len(mesh_grids_values) != len(self.mesh_positions):
+            raise ValueError(
+                "The provided mesh grid values do not match the number of grid dimensions.")
+
+        # Set the mesh grid values
+        self.mesh_grids_values = np.array(mesh_grids_values)
 
     def set_from_function(self, func: Callable[[tuple[float, ...]], tuple[float, ...]]) -> None:
         """
@@ -57,7 +78,7 @@ class VectorField:
             ValueError: If the function doesn't accept the same number of arguments as grid dimensions.
         """
         # Check if the function can accept the same number of arguments as the grid dimensions
-        required_args = len(self.mesh_points)
+        required_args = len(self.mesh_positions)
         func_args = len(inspect.signature(func).parameters)
         if func_args != required_args:
             raise ValueError("Function should accept {} arguments, but it accepts {}.".format(
@@ -66,37 +87,30 @@ class VectorField:
         # Calculate grid values using the provided function
         self.mesh_grids_values = np.array(func(*self.mesh_grids))
 
-        # Sum the grid values to create a combined grid
-        self.combined_grid_values = np.sum(self.mesh_grids_values, axis=0)
-
-        # Create interpolators for each grid
-        self.interpolators = [RegularGridInterpolator(
-            tuple(self.mesh_points), grid_values) for grid_values in self.mesh_grids_values]
-
-    def get_vectors_at_points(self, points: Sequence[tuple[float, ...]]) -> Sequence[tuple[float, ...]]:
+    def get_vectors_at_positions(self, positions: Sequence[np.array]) -> Sequence[np.array]:
         """
-        Interpolate vectors at specified points within the grid.
+        Interpolate vectors at specified positions within the grid.
 
         Parameters
         ----------
-            points (Sequence[Tuple[float, ...]]): A sequence of points for which to interpolate vectors.
+            positions (Sequence[np.ndarray]): A sequence of NumPy arrays representing positions for which to interpolate vectors.
 
         Returns
         -------
-            Sequence[Tuple[float, ...]: A sequence of interpolated vectors at the specified points.
+            Sequence[np.ndarray]: A sequence of NumPy arrays containing the interpolated vectors at the specified positions.
 
         Raises
         ------
-            ValueError: If the dimensionality of the specified points doesn't match the grid dimensions.
+            ValueError: If the dimensionality of the specified positions doesn't match the grid dimensions.
         """
-        # Check the points for dimensionality
-        if any(len(point) != len(self.mesh_points) for point in points):
+        # Check the positions for dimensionality
+        if any(len(position) != len(self.mesh_positions) for position in positions):
             raise ValueError(
-                "Point dimension does not match the grid dimension.")
+                "position dimension does not match the grid dimension.")
 
-        # Interpolate vectors at the specified points using the interpolators
-        interpolated_vectors = [interpolator(
-            points) for interpolator in self.interpolators]
+        # Interpolate vectors at the specified positions using the interpolators
+        interpolated_vectors = [interpn(
+            self.mesh_positions, values, positions, bounds_error=False) for values in self.mesh_grids_values]
 
-        # Transpose the result to have one tuple per point
-        return tuple(map(tuple, np.array(interpolated_vectors).T))
+        # Transpose the result to have one NDArray per position
+        return np.array(interpolated_vectors).T
