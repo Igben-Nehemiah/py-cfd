ad2764f8b5701291f33306fe7bddbc3218ad49be
Nehemiah Igben <igbennehemiah@gmail.com>
2023-10-16 20:03:51 +0100

    added title_size

--- particle_advection/plot_config.py
+++ particle_advection/plot_config.py
@@ -4,14 +4,16 @@ import seaborn as sns
 
 class PlotConfig:
     def __init__(self, size: tuple, 
-                 title_size: int=12,
-                 label_size: int=10,
-                 bg_color: str="#aaaaaa",
+                 title_size: int=8,
+                 axes_title_size: int=7,
+                 label_size: int=7,
+                 bg_color: str="#000",
                  num_colors: int=8,
                  aspect_ratio=1.0):
 
         self.size = size
         self.title_size = title_size
+        self.axes_title_size = axes_title_size
         self.label_size = label_size
         self.bg_color = bg_color
         self.num_colors = num_colors
